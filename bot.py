e383c15ecd9b37a9d901d30315d0b1cee1263ebe
Logan Selley <logans24@uw.edu>
2019-05-02 14:48:47 -0700

    Implement skip, asyncio queue, now playing

--- bot.py
+++ bot.py
@@ -4,27 +4,26 @@ import youtube_dl
 import asyncio
 import spotipy
 from discord.ext import commands
-from discord.utils import get
 
 '''
     Commands to add:
         change prefix                       !pre                                        In Progress
-        youtube/spotify play/search         !p  !play [required argument]               In Progress
+        youtube/spotify play/search         !p  !play [required argument]               1.5/3
         queue                               !q  !queue                                  In Progress
         lyrics of now playing/given song    !ly !lyrics [optional argument]
-        display all commands                !help  !commands
+        display all commands                !help
         join/disconnect                     !j/!l   !join/!leave                        COMPLETE
-        now playing                         !np !so  !nowplaying !song
+        now playing                         !np !so  !nowplaying !song                  COMPLETE
         looping playlist/queue/song         !loop   [required argument]
         remove song                         !re !remove [required argument]
         seek to certain point of song       !seek   [required argument]
-        pause/resume                        !pa/!r  !pause/!resume
-        skip/skipto                         !s  !skip   [optional argument]
+        pause/resume                        !pa/!r  !pause/!resume                      COMPLETE
+        skip/skipto                         !s  !skip   [optional argument]             1/2
         forward/rewind                      !f/!rw  !forward/!rewind    [required argument]
         move song position in queue         !move   [required argument] [required argument]
         clear queue                         !c  !clear
         remove duplicates                   !dupe   !d
-        volume                              !v  !volume     [required argument]
+        volume                              !v  !volume     [required argument]         COMPLETE
         shuffle                             !shuff  !shuffle
         play: add to top of queue           !p/!play [required argument] [required argument]
         play: add to top of queue and skip current  !p/!play [required argument] [required argument]
@@ -56,10 +55,9 @@ ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 '''API keys'''
 spotify = None
-youtube = None
 prefix = '!'
-players = {}
-'''Queue storage'''
+
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix),  case_insensitive=True)
 
 
@@ -84,8 +82,26 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 
 class Music(commands.Cog):
+    songs = asyncio.Queue()
+    play_next = asyncio.Event()
+    voice = None
+    server = None
+    player = None
+
     def __init__(self, bot):
         self.bot = bot
+        bot.loop.create_task(self.audio_player_task())
+
+    async def audio_player_task(self):
+        while True:
+            self.play_next.clear()
+            self.player = await self.songs.get()
+            self.voice.play(self.player, after=lambda e: print('Player error: %s' % e) if e else None)
+            await self.server.send('Now playing: {}'.format(self.player.title))
+            await self.play_next.wait()
+
+    def toggle_next(self):
+        bot.loop.call_soon_threadsafe(self.play_next.set)
 
     @commands.command(pass_context=True, name='join', aliases=['j'])
     async def join(self, ctx):
@@ -113,9 +129,10 @@ class Music(commands.Cog):
     async def play(self, ctx, url):
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(player.title))
+            await self.songs.put(player)
+        self.voice = ctx.voice_client
+        self.server = ctx
+        await ctx.message.delete()
 
     @play.before_invoke
     async def ensure_voice(self, ctx):
@@ -146,6 +163,24 @@ class Music(commands.Cog):
     async def stop(self, ctx):
         ctx.voice_client.stop()
 
+    @commands.command(pass_context=True, name='queue', aliases=['q'])
+    async def queue(self, ctx):
+        ctx.send(self.songs)
+
+    @commands.command(pass_context=True, name='skip', aliases=['s'])
+    async def skip(self, ctx):
+        self.voice.stop()
+        self.play_next.set()
+
+    @commands.command(pass_context=True, name='now playing', aliases=['np'])
+    async def now_playing(self, ctx):
+        try:
+            await ctx.send('Now playing: {}'.format(self.player.title))
+        except AttributeError:
+            await ctx.send('Not currently playing')
+
+
+
 
 @bot.event
 async def on_ready():
@@ -167,11 +202,6 @@ async def pre(ctx, fix):
 
 
 @bot.command()
-async def queue(ctx):
-    print('queue')
-
-
-@bot.command()
 async def lyrics(ctx, arg):
     if arg is None:
         print('given song lyrics')
@@ -184,4 +214,4 @@ async def shuffle(self, ctx):
     print('shuffle')
 
 bot.add_cog(Music(bot))
-bot.run('NTcwMTIxNzQzODk3ODUzOTgw.XMsn4g.Cee1NTgO9TSTa7TaumWeJl_f53A')
+bot.run('')
